0e539bde8904a5612b4875d940752e886753b630
Dara218 <ralphzacbautista@gmail.com>
2023-01-13 19:55:28 +0800

    updated codepen

--- rc.js
+++ rc.js
@@ -21,30 +21,6 @@ $(document).ready(function(){
         const rightPanel = $('.right-panel a')
         const aLink = $('.center-container3 p a')
         const hamburgerMenu = $('.hamburger-menu')
-        let currentScroll = scrollY
-
-        if(lastScroll > currentScroll){
-            mainNavEl.css({
-                top: '0'
-            })
-
-            hamburgerMenu.css({
-                top: '60px'
-            })
-        }
-
-        else{
-            mainNavEl.css({
-                top: '-100px'
-            })
-
-            hamburgerMenu.css({
-                top: '-200px'
-            })
-
-        }
-
-        lastScroll = currentScroll
 
         if(scrollY >= centerContainer1){
             leftPanel.hide()
@@ -70,6 +46,31 @@ $(document).ready(function(){
             leftPanel.removeClass('changeColorText')
             rightPanel.removeClass('changeColorText')
         }
+
+        let currentScroll = scrollY
+
+        if(lastScroll > currentScroll){
+            mainNavEl.css({
+                top: '0'
+            })
+
+            hamburgerMenu.css({
+                top: '60px'
+            })
+        }
+
+        else{
+            mainNavEl.css({
+                top: '-100px'
+            })
+
+            hamburgerMenu.css({
+                top: '-200px'
+            })
+
+        }
+
+        lastScroll = currentScroll
     })
 
     const hamburgerBtn = $('.fa-bars')
